a91b55b1147ea2f0bf4a7c4f3c31d5860024d412
Oyjord <oyjord@gmail.com>
2025-10-06 12:50:21 -0700

    Update scrape_reign_schedule.rb

--- scrape_reign_schedule.rb
+++ scrape_reign_schedule.rb
@@ -24,13 +24,21 @@ def parse_goal_scorers(report_url, home_team, away_team)
 
       # Fallback parser: "3, Ontario, Chromiak 1 14:44(SH)"
       if tokens.size == 3 && tokens[2].include?(':') && tokens[2].include?('(')
+  period_number = tokens[0]
   team = tokens[1]
   scorer_parts = tokens[2].split(/\s+/)
   scorer = scorer_parts[0..-3].join(' ')
   time = scorer_parts[-2]
   strength = scorer_parts[-1].gsub(/[()]/, '')
 
-  entry = "#{scorer} (#{time})"
+  period_label = case period_number
+                 when "1" then "1st Period"
+                 when "2" then "2nd Period"
+                 when "3" then "3rd Period"
+                 else nil
+                 end
+
+  entry = "#{scorer} (#{period_label} #{time})"
   entry += " [#{strength}]" if strength && !strength.empty?
 
   if team == home_team
